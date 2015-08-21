5890ea70ac3f3834cb1b0f34e0b9e1129b2ce2fd
Alex Myasoedov <msoedov@gmail.com>
2015-08-21 21:18:16 +0300

    Roadmap.md

--- broccoli/__init__.py
+++ broccoli/__init__.py
@@ -1,20 +1,2 @@
 from .core import *
 from .flask import *
-
-# todo:
-# - make file
-# - unitests
-# - docstrings
-# - decorators?
-# - rename
-# - inject by package?
-# - pretify errros?
-# - samples
-# - setup.py package
-# - log levels
-# - readme
-# - CI
-# - tox
-# optional todo:
-# - method annotation
-# - ...
---
+++ roadmap.md
@@ -0,0 +1,17 @@
+# todo:
+ - make file
+ - unitests
+ - docstrings
+ - decorators?
+ - rename
+ - inject by package?
+ - pretify errros?
+ - samples
+ - setup.py package
+ - log levels
+ - readme
+ - CI
+ - tox
+ optional todo:
+ - method annotation
+ - ...

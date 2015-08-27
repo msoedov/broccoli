bb18cfb71a374711dd0d2e679af03b625b01c1f1
Alex Myasoedov <msoedov@gmail.com>
2015-08-27 19:51:30 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -29,18 +29,21 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries',
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     # What does your project relate to?
     keywords='dependency injection',

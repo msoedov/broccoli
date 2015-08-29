79cffea5b4c7fd8a38e1482bd38e7f8ba7843c1b
Alex Myasoedov <msoedov@gmail.com>
2015-08-29 20:01:34 +0300

    Inject by module

--- inject.py
+++ inject.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 import inspect
+import sys
+import types
 
 
 Db = namedtuple('Db', '')
@@ -15,8 +17,27 @@ def request(request, db: Db, session: Session, service: Service):
     print(request, db, session, service)
 
 
-def inject(fn):
-    deps = new_app()
+def update_query(db: Db):
+    print(db)
+
+
+def inject(module, *deps):
+    if isinstance(module, str):
+        module = sys.modules[module]
+    fn_list = []
+    for attr in vars(module).values():
+        if isinstance(attr, types.FunctionType):
+            fn_list.append(attr)
+    for fn in fn_list:
+        injected, vals = bind(fn, *deps)
+        if injected:
+            print("Injected {} -> {}".format(vals, fn.__name__))
+
+
+def bind(fn, *deps):
+    """
+    :param: fn - function or method
+    """
     type_to_dependency = {type(d): d for d in deps}
     detected_dependecies = {}
     signature = inspect.signature(fn)
@@ -29,12 +50,14 @@ def inject(fn):
     for param in signature.parameters:
         if param in detected_dependecies:
             defaults.append(detected_dependecies[param])
+    if not defaults:
+        return False, []
     fn.__defaults__ = tuple(defaults)
-    return fn
+    return True, defaults
 
 
-inject(request)
+inject('__main__', *new_app())
 
 request('foo')
-
-print(self)
+update_query()
+update_query('MockDb')
